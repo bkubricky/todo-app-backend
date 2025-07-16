4097f87847f2c3bc3ada365717a2e96972806bec
benjamin kubricky <bkubricky@gmail.com>
2025-07-15 19:30:53 -0700

    todo app backend

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var router = express.Router();
+const mongoose = require("mongoose");
+const Todos = require("./models/Todos");
+require("dotenv").config();
+
+// Body parsing middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+mongoose.connect(`${process.env.MONGODB_URI}`);
+
+/* GET home page. */
+router.get("/", async function (req, res, next) {
+  const todoList = await Todos.find();
+  res.send(todoList);
+});
+
+router.post("/new", async function (req, res) {
+  await Todos.create(req.body);
+  res.status(200);
+});
+
+router.post("/:id/update", async function (req, res) {
+  const { id } = req.params;
+  await Todos.findOneAndUpdate({ id: id }, req.body);
+  res.status(200);
+});
+
+router.post("/:id/delete", async function (req, res) {
+  const { id } = req.params;
+  await Todos.deleteOne({ id: id });
+  res.status(200);
+});
+module.exports = router;
